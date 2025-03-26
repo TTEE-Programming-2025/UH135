4fd15df1ed59c4373ea5344c60bbf52919d2399f
Ice7709 <u11319035@o365.ttu.edu.tw>
2025-03-27 01:03:03 +0800

    first version:寫了一隻貓，拿著旗子，上面寫你好

---
+++ P1UH135.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+printf("********************\n");
+printf("個人風格畫面\n");
+printf("********************\n");
+ printf("～～～＼\n");
+    printf("｜你好！＼\n");
+    printf("～～～～～＼\n");
+    printf("　　　　　　＼\n");
+    printf("　　　　\"∧＿＿∧∩\n");
+    printf("　　　　(*　>ω•́ )ﾉ＼\n");
+    printf("ｰ=≡t―ｰ/ヽ＿つ）_s)\n");
+    printf("≡(〓〓(　)／＼＼ｰ＼\n");
+    printf("=≡( (〓:(／　|（Ｏ)T\n");
+    printf("=≡ヽ､＿ノ￣￣ヽ＿／\n");
+system("pause");
+return 0;
+}
