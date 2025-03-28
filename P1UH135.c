40ba6c4577b8d4ee7a3f3f21ca60517459c39fc6
Ice7709 <u11319035@o365.ttu.edu.tw>
2025-03-28 13:37:00 +0800

    個人介面增加一個新圖案並增加按鍵清除螢幕功能

--- P1UH135.c
+++ P1UH135.c
@@ -2,10 +2,13 @@
 #include<stdlib.h>
 int main()
 {
-printf("********************\n");
-printf("個人風格畫面\n");
-printf("********************\n");
- printf("～～～＼\n");
+    
+    for(int i=0;i<=100;i++)
+    {
+      printf("=");
+    }
+    printf("\n");
+    printf("～～～＼\n");
     printf("｜你好！＼\n");
     printf("～～～～～＼\n");
     printf("　　　　　　＼\n");
@@ -15,6 +18,24 @@ printf("********************\n");
     printf("≡(〓〓(　)／＼＼ｰ＼\n");
     printf("=≡( (〓:(／　|（Ｏ)T\n");
     printf("=≡ヽ､＿ノ￣￣ヽ＿／\n");
+    printf("                              ～～～～～～～～～＼\n");
+    printf("                              ｜請按按鍵清除螢幕! ＼\n");
+    printf("                              ～～～～～～～～～～～＼\n");
+    printf("　　　　　                                          　＼\n");
+    printf("　　　　                                         \"∧＿＿∧∩\n");
+    printf("　　　                                         　(*　>ω< )ﾉ＼\n");
+    printf("                                         ｰ=≡t―ｰ/ヽ＿つ）_s)\n");
+    printf("                            ≡≡≡≡≡≡≡≡     ≡(〓〓(　)／＼＼ｰ＼\n");
+    printf("            ==≡≡≡≡≡≡≡≡      ====≡≡≡      =≡( (〓:(／　|（Ｏ)T\n");
+    printf("===≡≡≡≡     ====≡≡≡≡≡≡≡≡    ====≡≡         =≡ヽ､＿ノ￣￣ヽ＿／\n");
+    for(int i=0;i<=100;i++)
+    {
+      printf("=");
+    }
+    printf("\n");
+    getchar();
+    system("CLS");
+
 system("pause");
 return 0;
 }
