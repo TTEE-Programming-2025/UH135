636dc99a1c2256af1ec4c91e6a9d2ef8c00d3fdf
Ice7709 <u11319035@o365.ttu.edu.tw>
2025-04-27 15:53:17 +0800

    在最後註解新增心得，並修改使用按鍵a和c功能時，若輸入複數字元會有bug的問題

--- P2UH135.c
+++ P2UH135.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-//宣告直線函數
+/*宣告直線函數*/
 void printLine(char ch) {
     for (int i = 0; i <=100 ; i++) {
         printf("%c", ch);
@@ -73,6 +73,9 @@ int main(){
                         do {
                             printf("請輸入字元a-n: ");
                             scanf(" %c", &ch);
+                            //清除緩衝內換行符號
+                            while (getchar()!= '\n');
+
                             if (ch >= 'a' && ch <= 'n') {
                                 int h = ch - 'a' + 1;
                                 for (int i = 0; i < h; i++) {
@@ -90,7 +93,7 @@ int main(){
                             } else {
                                 printf("錯誤!請輸入字元a-n\n");
                             }
-                        } while (1);
+                        } while (ch < 'a' || ch > 'n');
                         printf("按任意鍵回到主選單\n");
                         getch();
                         system("cls");
@@ -131,6 +134,9 @@ int main(){
                         do {
                             printf("Continue? (y/n): ");
                             scanf(" %c", &yn);
+                            //清除緩衝內換行符號
+                            while (getchar()!= '\n');
+
                             if (yn == 'y' || yn == 'Y') {
                                 system("cls");
                                 system("clear");
@@ -162,3 +168,15 @@ int main(){
     system("pause");
     return 0;
 }
+
+
+/*心得:
+這次作業我一開始沒看好作業要求，導致後面花了很多時間重寫選單功能。下次會更加注意作業要求，避免
+像這次一樣內容需要大改。此外，這次在撰寫個人風格畫面時，設置函數讓顯示直線更加簡潔，不必再看到
+複雜的for迴圈，而是使用自己設定的printLine，還能快速地更改需要顯示100個的符號。同時，這次撰寫因
+為需要大改的關係，讓我更加熟悉vim的快捷鍵使用，在更快速的修改程式方面幫了大忙。
+
+選單功能需要不斷重複輸入，這次嘗試使用while(1)，而不是使用以前慣用的do-while，我覺得while(1)在使
+用上更加有彈性，do-while比較適合輸入驗證，while(1)則更方便在各部分保證選單出現，適合像這次這種無
+限選單，但while(1)的可讀性較差，在檢查程式時給我帶來一些困擾。不過之後應該也會常常使用這兩種迴圈
+，希望能夠更加熟悉如何用好while(1)迴圈。*/
